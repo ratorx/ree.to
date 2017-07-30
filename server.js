1f26d3829ccacf6520852060a1f72bd795cf4e17
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2017-07-30 18:12:57 +0100

    Add webserver

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const
     express = require("express"),
-    https = require("https");
+    // https = require("https"),
+    path = require("path");
+
+    var app = express();
+
+    // Main Page
+    app.use("/", express.static(path.join(__dirname, "public")));
+    
+    // 404
+    app.use(function(req, res) {
+        res.status(404);
+        res.sendFile(path.join(__dirname, "public/404.html"));
+    })
+
+    app.listen(80, () => console.log("Started server on port 80"));
